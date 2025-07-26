9158935fa2c9e6e0e1d35bf67a97ae8f48b46f44
anil kumar <akthinq@gmailcom>
2025-07-26 13:29:10 +0530

    var

--- 00_shell_variables.sh
+++ 00_shell_variables.sh
@@ -4,10 +4,15 @@ HELLO="Hello worlid!"
 
 echo $HELLO
 
+echo "exit status"
 echo $?
 
+echo "PID of current script"
 echo $$
 
+sleep 40&
+echo "PID of last command running on background"
 echo $!
 
+echo "srcipt name"
 echo $0
