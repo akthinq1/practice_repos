7846904f8e8369d5c13aba46034970755f60511f
anil kumar <akthinq@gmailcom>
2025-07-26 16:34:00 +0530

    var

--- 02_shell_move_logs.sh
+++ 02_shell_move_logs.sh
@@ -3,11 +3,12 @@
 USER=$(id -u)
 LOGS_FOLDER="/var/log/shell_logs"
 SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
-LOG_FILE="LOGS_FOLDER/SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 if [ $USER != 0 ]
 then
     echo "run user with root access"
+    exit 1
 else
     echo "script started executing"
 fi
