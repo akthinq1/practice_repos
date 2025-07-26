18db204010ad08b9824b8090cd3332773eccfdcc
anil kumar <akthinq@gmailcom>
2025-07-26 16:28:39 +0530

    var

--- 02_shell_move_logs.sh
+++ 02_shell_move_logs.sh
@@ -5,13 +5,13 @@ LOGS_FOLDER="/var/log/shell_logs"
 SCRIPT_NAME="$(echo $0 | cut -d "." f1)"
 LOG_FILE="LOGS_FOLDER/SCRIPT_NAME.log"
 
-if [ $user != 0 ]
+if [ $USER != 0 ]
 then
     echo "run user with root access"
 else
     echo "script started executing"
 fi
 
-mkdir -p $LOGS_FOLDER &>>$LOG_FILE
+mkdir -p $LOGS_FOLDER
 
 dnf install nginx -y | tee -a $LOG_FILE
